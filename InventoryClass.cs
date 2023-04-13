64b683ec8b936977332ce9506e1a07a0fa211e0f
samirasingh19 <samira.singh021@gmail.com>
2023-04-13 18:18:27 +0530

    InventoryClass

---
+++ InventoryClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concept
+{
+    public class InventoryClass
+    {
+        public List<TypesOfRice> typesOfRice;
+        public List<TypesOfWheet> typesOfWheet;
+        public List<TypesOfPulses> typesOfPulses;
+    }
+
+    public class TypesOfRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfWheet
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfPulses
+    {
+        public string name;
+        public int weight;
+        public int price
+    }
+}
+
