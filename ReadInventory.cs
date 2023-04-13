171050e9d425d8d240170220ae61069b075a5253
samirasingh19 <samira.singh021@gmail.com>
2023-04-13 18:20:07 +0530

    ReadInventory

---
+++ ReadInventory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace OOP_concept
+{
+    public class ReadInventory
+    {
+
+        public InventoryDetails Read(string path)
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(path))
+                {
+                    string jason = file.ReadToEnd();
+
+                    return JasonConvert.DeserializeObject<InventoryDetails>(json);
+                }
+            }
+        Catch(Exception e)
+        {
+                return null;
+            }
+
+        }
+
+    }
+}
